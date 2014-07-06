961c067711f5eef78f930aab50d67e4e6570423e
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-07-06 21:42:46 +0700

    Add functions: create_unix_{stream,datagram}_socket

--- lib/SHARYANTO/IO/Socket/UNIX/Util.pm
+++ lib/SHARYANTO/IO/Socket/UNIX/Util.pm
@@ -14,10 +14,14 @@ use Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(
                        create_unix_socket
+                       create_unix_stream_socket
+                       create_unix_datagram_socket
                );
 
 sub create_unix_socket {
-    my ($path, $mode) = @_;
+    my ($path, $mode, $opts) = @_;
+
+    $opts //= {};
 
     my $old_locale = setlocale(LC_ALL);
 
@@ -29,6 +33,7 @@ sub create_unix_socket {
         $sock = IO::Socket::UNIX->new(
             Type => SOCK_STREAM,
             Peer => $path,
+            %$opts,
         );
         my $err = $@ unless $sock;
         if ($sock) {
@@ -50,6 +55,7 @@ sub create_unix_socket {
         Type   => SOCK_STREAM,
         Local  => $path,
         Listen => 1,
+        %$opts,
     );
     die "Can't create listening Unix socket: $@" unless $sock;
 
@@ -60,18 +66,49 @@ sub create_unix_socket {
     $sock;
 }
 
+sub create_unix_stream_socket {
+    my ($path, $mode, $opts) = @_;
+    $opts //= {};
+    create_unix_socket($path, $mode, {Type=>SOCK_STREAM, %$opts});
+}
+
+sub create_unix_datagram_socket {
+    my ($path, $mode, $opts) = @_;
+    $opts //= {};
+    create_unix_socket($path, $mode, {Type=>SOCK_DGRAM, %$opts});
+}
+
 1;
 # ABSTRACT: Unix domain socket utilities
 
 =head1 FUNCTIONS
 
-=head2 create_unix_socket($path[, $mode]) => SOCKET
+=head2 create_unix_socket($path[, $mode, \%opts]) => SOCKET
 
-Create a listening Unix socket. Die on failure.
+Create a listening Unix socket (by default with Type SOCK_STREAM) using
+L<IO::SOcket::UNIX>. C<%opts> will be passed to L<IO::Socket::UNIX>.
 
-This function creates Unix domain socket with L<IO::Socket::UNIX> with some
-extra stuffs: remove stale socket first, show more detailed/precise error
-message, chmod with $mode.
+Die on failure.
+
+This function creates Unix domain socket with the usual way of using
+L<IO::Socket::UNIX> with some extra stuffs: remove stale socket first, show more
+detailed/precise error message, chmod with $mode.
+
+
+=head2 create_unix_stream_socket($path[, $mode, \%opts]) => SOCKET
+
+Shortcut for:
+
+ create_unix_socket($path, $mode, {Type=>SOCK_STREAM, %opts});
+
+which is the default anyway.
+
+
+=head2 create_unix_datagram_socket($path[, $mode, \%opts]) => SOCKET
+
+Shortcut for:
+
+ create_unix_socket($path, $mode, {Type=>SOCK_DGRAM, %opts});
 
 
 =head1 SEE ALSO
